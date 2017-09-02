dc64a860f8b46c2b62e76303df3d1cf9c0d32552
d-dtm <dylan.menchetti@gmail.com>
2017-09-02 14:52:34 -0400

    GitHub SyncSolve
    Working on the Github merge fetches

--- src/LostFriend1.java
+++ src/LostFriend1.java
@@ -1,8 +1,8 @@
 import java.util.ArrayList;
 
 public class LostFriend1 {
-<<<<<<< HEAD
-	private PathHolder pathTaken;
+
+	private Path pathTaken;
 	private int[] startingPoint;
 	private ArrayList<int[]> surrounding;
 	private String direction;
@@ -17,17 +17,7 @@ public class LostFriend1 {
 	}
 
 	public ArrayList<int[]>() getSurroundings() {
-=======
-	private Path pathTaken;
-	private Path initial;
-	private ArrayList<int[]> surroudning;
-	
-	public LostFriend1(Path pT, Path iNi ) {
-		this.pathTaken = pT;
-		this.initial = iNi;
->>>>>>> 9f4fe180a926d78745344635c1440a32b04728fe
-		
-	}
+
 	
 
 public static void main(String[] args) {
