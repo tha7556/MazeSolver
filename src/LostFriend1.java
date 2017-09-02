181b8f0cb72a6c89c8802c4484beecb0f33c99c6
d-dtm <dylan.menchetti@gmail.com>
2017-09-02 14:48:16 -0400

    lostFriend
    LostFriend initiliaze at start, begin surrounding/orientation work.

--- src/LostFriend1.java
+++ src/LostFriend1.java
@@ -2,15 +2,31 @@ import java.util.ArrayList;
 
 public class LostFriend1 {
 	private PathHolder pathTaken;
-	private PathHolder initial;
-	private ArrayList<int[]> surroudning;
+	private int[] startingPoint;
+	private ArrayList<int[]> surrounding;
+	private String direction;
+	private boolean[] maze;
 	
-	public LostFriend1(PathHolder pT, PathHolder iNi ) {
-		this.pathTaken = pT;
-		this.initial = iNi;
+	
+	public LostFriend1(int x, int y, boolean[][] startMaze) {
+		this.startingPoint = new int[] {x,y};
+		surrounding = new ArrayList<int[]>();
+		
+		
+	}
+
+	public ArrayList<int[]>() getSurroundings() {
 		
 	}
 	
+
+public static void main(String[] args) {
+	Maze x = new Maze("C:\\\\Documents\\\\GitHub\\\\MazeSolver\\\\Maze.png");
+	
+	LostFriend1 bob = new LostFriend1(0,3, x.getArray());
+	
+	
+}
 	
 	
 }
