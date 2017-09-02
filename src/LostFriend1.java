7eb984e8379e9f3615afe085c8fd686290aa5bff
d-dtm <dylan.menchetti@gmail.com>
2017-09-02 13:55:48 -0400

    Runner+Path
    Begin Constructors for path and runner

--- src/LostFriend1.java
+++ src/LostFriend1.java
@@ -1,5 +1,16 @@
+import java.util.ArrayList;
 
 public class LostFriend1 {
-	private int pathTaken;
+	private PathHolder pathTaken;
+	private PathHolder initial;
+	private ArrayList<int[]> surroudning;
+	
+	public LostFriend1(PathHolder pT, PathHolder iNi ) {
+		this.pathTaken = pT;
+		this.initial = iNi;
+		
+	}
+	
+	
 	
 }
--- src/PathHolder.java
+++ src/PathHolder.java
@@ -1,4 +1,34 @@
+import java.util.ArrayList;
+
 
 public class PathHolder {
-
+	private int xCord;
+	private int yCord;
+	private ArrayList<int []> holdMe;
+	
+	
+	public PathHolder(int x, int y) {
+		this.xCord = x;
+		this.yCord = y;
+		holdMe = new ArrayList<int []>();
+		
+	}
+	
+	
+	public int xGetter() {
+		return xCord;
+		
+	}
+	
+	public int yGetter() {
+		return yCord;
+		
+	}
+	
+	public ArrayList<int[]> arrayGetter() {
+		return holdMe;
+		
+	}
+	
+	
 }
