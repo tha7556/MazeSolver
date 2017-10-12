e0119fe33323e9cfcbc24f6078fa5cfe98553bac
Tyler <tha7556@users.noreply.github.com>
2017-10-12 13:26:32 -0400

    Made LostFriend abstract

--- src/mazeSolver/LostFriend.java
+++ src/mazeSolver/LostFriend.java
@@ -5,27 +5,29 @@ import java.util.ArrayList;
 import mazeGenerator.Maze;
 import mazeGenerator.Point;
 
-public class LostFriend {
+public abstract class LostFriend {
 
-	private Path pathTaken;
-	private Point startingPoint;
-	private Point currentPoint;
-	private String direction; //TODO: Method to find direction
-	private Point north, south, east, west, end;
-	private Maze maze;
+	protected Path pathTaken;
+	protected Point startingPoint;
+	protected Point currentPoint;
+	protected String direction; //TODO: Method to find direction
+	protected Point north, south, east, west, end;
+	protected Maze maze;
 	
 	public LostFriend(int startx, int starty, int endx, int endy, Maze startMaze) { //TODO: Make this class the generic friend (Abstract Classes), extend it into 3 others for the algorithms?
 		this.maze = startMaze;
 		this.startingPoint = maze.getPoint(startx,starty);
 		this.currentPoint = maze.getPoint(startx,starty);
+		System.out.println(endy);
 		end = maze.getPoint(endx, endy);
 		System.out.println(startingPoint.getY());
 		this.startingPoint.changeColor(Color.BLUE);
 		end.changeColor(Color.red);
 		getSurroundings();
+		pathTaken = new Path(maze);
 		}
 
-	public void getSurroundings() {
+	public Point[] getSurroundings() {
 		int x = currentPoint.getX();
 		int y = currentPoint.getY();
 		int mazeWidth = maze.getMazeWidth();
@@ -46,27 +48,28 @@ public class LostFriend {
 			south = maze.getPoint(x,y+1);
 		else
 			south = null;
+		return new Point[]{north,east,south,west};
 	}
 	public ArrayList<Point> getAvailablePoints() {
+		getSurroundings();
 		ArrayList<Point> points = new ArrayList<Point>();
-		if(getNorth() != null && !getNorth().isBlocked())
-			points.add(getNorth());
-		if(getSouth() != null && !getSouth().isBlocked())
-			points.add(getSouth());
-		if(getEast() != null && !getEast().isBlocked())
-			points.add(getEast());
-		if(getWest() != null && !getWest().isBlocked())
-			points.add(getWest());
+		if(north != null && !north.isBlocked() && !north.hasTraveled())
+			points.add(north);
+		if(south != null && !south.isBlocked() && !south.hasTraveled())
+			points.add(south);
+		if(east != null && !east.isBlocked() && !east.hasTraveled())
+			points.add(east);
+		if(west != null && !west.isBlocked() && !west.hasTraveled())
+			points.add(west);
+		
 		return points;
 	}
 	public void moveTo(Point point) {
-		//TODO: Implement this to move the Friend, and handle pathTaken
 		if (getAvailablePoints().contains(point)) {
 			this.currentPoint = maze.getPoint(point.getX(), point.getY());
 			point.setTraveled(true);
 			point.changeColor(Color.BLUE);
-			pathTaken.addPoint(point.getX(), point.getY());
-			maze.getDisplay().repaint();
+			getSurroundings();
 		}
 		else
 			System.out.println("Cannot move to point at: ("+point.getX()+","+point.getY()+")");
@@ -89,10 +92,18 @@ public class LostFriend {
 	public Point getCurrentPoint() {
 		return currentPoint;
 	}
+	public Path solveMaze() {
+		while(!currentPoint.equals(end)) {
+			Point next = calculateMove();
+			moveTo(next);
+		}
+		return pathTaken;
+	}
+	public abstract Point calculateMove();
 	public static void main(String[] args) {
 		//Maze x = new Maze("C:\\\\Documents\\\\GitHub\\\\MazeSolver\\\\Maze.png");
 		Maze x = new Maze("E:\\\\Computer Science\\\\GitHub\\\\MazeSolver\\\\Mazes\\\\Small\\\\maze1.png");
-		LostFriend bob = new LostFriend(1,1,x.getMazeWidth()-2, x.getMazeHeight() - 2, x);
+		//LostFriend bob = new LostFriend(1,1,x.getMazeWidth()-2, x.getMazeHeight() - 2, x);
 		
 		
 	}
